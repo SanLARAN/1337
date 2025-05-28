210676fa0e60783ef4f81f687a8d5b4aec4ca17a
SanLARAN <136124874+SanLARAN@users.noreply.github.com>
2025-05-29 00:32:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const header = document.getElementById('header');
-
-function updateHeaderStyle() {
+window.addEventListener('scroll', () => {
   if (window.scrollY > 10) {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
   }
-}
-
-window.addEventListener('scroll', updateHeaderStyle);
-window.addEventListener('load', updateHeaderStyle);
+});
